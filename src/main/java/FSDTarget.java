baa04b0264744ec28bc7d86745acc251189578a2
RuairidhMcCrindle <ruarty123@gmail.com>
2025-08-12 23:49:54 +0100

    added FSDTarget record

---
+++ src/main/java/FSDTarget.java
@@ -0,0 +1,8 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record FSDTarget(String Name,
+                        long SystemAddress,
+                        String StarClass,
+                        String RemainingJumpsInRoute) {
+}
