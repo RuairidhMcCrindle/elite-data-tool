5d8aae3849df85b6e521940859292841657f29d2
RuairidhMcCrindle <ruarty123@gmail.com>
2025-08-13 19:31:24 +0100

    capitalisation for serializing in Loadout

--- src/main/java/events/loadout/Loadout.java
+++ src/main/java/events/loadout/Loadout.java
@@ -3,7 +3,7 @@ package events.loadout;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Loadout(String ship, int ShipID, String ShipName,
+public record Loadout(String Ship, int ShipID, String ShipName,
                       String ShipIdent, long ModulesValue, float HullHealth,
                       float UnladenMass, int CargoCapacity,
                       float MaxJumpRange, long Rebuy, Module[] Modules) {
